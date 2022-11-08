7d16900674d1a257c78e11cbfd8267694f5f626e
hardikamareliya <hardikamareliya221@gmail.com>
2022-11-08 08:06:08 -0500

    Following changes are done: 1) Implemented Inheritance 2) Two table creation 3) One class for Main 4) Second class for Databse calls methods.

--- controller.py
+++ controller.py
@@ -5,91 +5,113 @@ import connection
 
 url = 'https://www.googleapis.com/books/v1/volumes?q=love:keyes&key=AIzaSyCB4mblXymeFZnTJKzp409fWKh8QC8Ty5Y&maxResults=30'
 
+class Main:
 
-def read_api():
-    global url
-    api_result = requests.get(url)
-    api_response = api_result.json()
-    # api_data = json.dumps(api_response, indent=4)
-    # new_data = json.loads(api_data)
-    # for i in range(len(new_data)):
-    #     print(new_data['items'][i]['id'])
+    def read_api(self):
+        global url
+        api_result = requests.get(url)
+        api_response = api_result.json()
+        # api_data = json.dumps(api_response, indent=4)
+        # new_data = json.loads(api_data)
+        # for i in range(len(new_data)):
+        #     print(new_data['items'][i]['id'])
 
-    return api_response
-
-
-def write_data(api_response):
-    try:
-        # Downloading json data into a file
-        data = json.dumps(api_response)
-
-        file = open("api_data.json", "w")
-        file.write(data)
-        print("API data successfully downloaded!!")
-    except:
-        print("Error in File creation/ data insertion")
+        return api_response
 
 
-def connect():
-    # # connect to MySQL
-    # #con = pymysql.connect(host = 'localhost',user = 'root',passwd = '',db = 'test')
-    cursor = connection.db1.cursor()
-    return cursor
+    def write_data(self, api_response):
+        try:
+            # Downloading json data into a file
+            data = json.dumps(api_response)
+
+            file = open("api_data.json", "w")
+            file.write(data)
+            print("API data successfully downloaded!!")
+        except:
+            print("Error in File creation/ data insertion")
 
 
-def read_data():
-    f = open("api_data.json", "r")
-    new_data = json.loads(f.read())
-    return new_data
+    def connect(self):
+        # # connect to MySQL
+        # #con = pymysql.connect(host = 'localhost',user = 'root',passwd = '',db = 'test')
+        cursor = connection.db1.cursor()
+        return cursor
 
 
-def validate_string(val=None):
-    if val != None:
-        if type(val) is int:
-            # for x in val:
-            #   print(x)
-            return str(val).encode('utf-8')
+    def read_data(self):
+        f = open("api_data.json", "r")
+        new_data = json.loads(f.read())
+        return new_data
+
+
+    def validate_string(self, val=None):
+        if val != None:
+            if type(val) is int:
+                # for x in val:
+                #   print(x)
+                return str(val).encode('utf-8')
+            else:
+                return val
         else:
-            return val
-    else:
-        return None
+            return None
 
+class DbCalls(Main):
 
-def table_creation(cursor):
-    drop_book = "DROP TABLE IF EXISTS Book"
+    def __int__(self, cursor, ):
+        self.cursor=cursor
+        self.main=Main()
+    def table_creation(self, cursor):
+        drop_book = "DROP TABLE IF EXISTS Book"
+        drop_bookInfo = "DROP TABLE IF EXISTS BookInfo"
+        cursor.execute(drop_book)
+        cursor.execute(drop_bookInfo)
 
-    cursor.execute(drop_book)
-    cursor.execute("CREATE TABLE Book "
-                   "(BookId varchar(255) NOT NULL, "
-                   "selfLink varchar(255),"
-                   "PRIMARY KEY (BookId))")
+        cursor.execute("CREATE TABLE Book "
+                       "(bookId varchar(255) NOT NULL, "
+                       "type varchar(30), "
+                       "totalItems int, "
+                       "PRIMARY KEY(bookId))"
+                       )
+        cursor.execute("CREATE TABLE BookInfo "
+                       "(bookId varchar(255) NOT NULL, "
+                       "etag varchar(255), "
+                       "selfLink varchar(255), "
+                       "volume_id int, "
+                       "publish_id int, "
+                       "language varchar(30), "
+                       "isAvailablePDF BIT(1), "
+                       "PRIMARY KEY(bookId))"
+                       )
 
-def data_insertion_into_table(new_data):
-    # parse json data to SQL insert
-    for i in range(len(new_data)):
-        print(new_data['items'][i]['id'], new_data['items'][i]['selfLink'])
-        bookId = validate_string(new_data['items'][i]['id'])
-        selfLink = validate_string(new_data['items'][i]['selfLink'])
+    def data_insertion_into_table(self, new_data):
+        # parse json data to SQL insert
+        type = url.split('?')[1].split('=')[1].split(':')[0]
+        for i in range(len(new_data)):
+            print(new_data['items'][i]['id'], new_data['items'][i]['selfLink'])
+            bookId = main.validate_string(new_data['items'][i]['id'])
+            totalItem = main.validate_string(new_data['totalItems'])
 
-        cursor.execute("INSERT INTO Book (BookId, selfLink) VALUES (%s, %s)", (bookId, selfLink))
-    print('Data inserted successfully')
-    connection.db1.commit()
+            cursor.execute("INSERT INTO Book (bookId, type, totalItems) VALUES (%s, %s, %s)", (bookId, type, totalItem))
+        print('Data inserted successfully')
+        connection.db1.commit()
 
 if __name__ == '__main__':
     # Read API data into JSON
-    json_data = read_api()
+    main = Main()
+    json_data = main.read_api()
 
     # Create a api_data.json file and put downloaded data into that file
-    write_data(json_data)
+    main.write_data(json_data)
 
     # Connect with DB
-    cursor = connect()
+    cursor = main.connect()
 
     # Read data from downloaded file
-    data = read_data()
+    data = main.read_data()
 
     # Table creation
-    table_creation(cursor)
+    dbCalls = DbCalls()
+    dbCalls.table_creation(cursor)
 
     # insert data into table
-    data_insertion_into_table(data)
+    dbCalls.data_insertion_into_table(data)
