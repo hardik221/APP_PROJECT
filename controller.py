f3fcc60c654b5494315a8b1b3dc84e2caf07f0c0
Jeet Ambaliya <jeetambaliya44@gmail.com>
2022-11-10 19:33:58 -0500

    Separated drop, create and insert queries in methods.

--- controller.py
+++ controller.py
@@ -1,26 +1,24 @@
 import requests
 import json
-import pymysql
 import connection
 import random
 import uuid
+
 params_subject = ['love', 'feminism', 'inspirational', 'authors', 'fiction', 'poetry', 'fantasy', 'romance']
-
-url = f'https://www.googleapis.com/books/v1/volumes?q={str(random.choice(params_subject))}:keyes&key=AIzaSyCB4mblXymeFZnTJKzp409fWKh8QC8Ty5Y&maxResults=40'
-
 class Main:
     def read_api(self):
         global url
-        api_result = requests.get(url)
+        dbCalls = DbCalls()
+        dbCalls.table_creation(self.connect())
+        for item in params_subject:
+            url = f'https://www.googleapis.com/books/v1/volumes?q={str(item)}:keyes&key=AIzaSyCB4mblXymeFZnTJKzp409fWKh8QC8Ty5Y&maxResults=10'
 
-        api_response = api_result.json()
-        # api_data = json.dumps(api_response, indent=4)
-        # new_data = json.loads(api_data)
-        # for i in range(len(new_data)):
-        #     print(new_data['items'][i]['id'])
-        print("-----", api_response["items"][0])
-        return api_response["items"]
+            api_result = requests.get(url)
 
+            api_response = api_result.json()
+            self.write_data(api_response)
+            data = self.read_data()
+            dbCalls.data_insertion_into_table(data, self.connect())
     def write_data(self, api_response):
         try:
             # Downloading json data into a file
@@ -30,18 +28,14 @@ class Main:
             print("API data successfully downloaded!!")
         except:
             print("Error in File creation/ data insertion")
-
     def connect(self):
-        # # connect to MySQL
-        # #con = pymysql.connect(host = 'localhost',user = 'root',passwd = '',db = 'test')
+        # connect to MySQL
         cursor = connection.db1.cursor()
         return cursor
-
     def read_data(self):
         f = open("api_data.json", "r")
         new_data = json.loads(f.read())
-        return new_data
-
+        return new_data['items']
     def validate_string(self, val=""):
         if val != "":
             if type(val) is int:
@@ -53,13 +47,11 @@ class Main:
         else:
             return ""
 
-
 class DbCalls(Main):
-    def __int__(self,cursor,):
+    def __int__(self, cursor):
         self.cursor = cursor
         self.main = Main()
-
-    def table_creation(self, cursor):
+    def drop_if_exists(self, cursor):
         drop_book = "DROP TABLE IF EXISTS Book"
         drop_bookInfo = "DROP TABLE IF EXISTS BookInfo"
         drop_VolumeInfo = "DROP TABLE IF EXISTS VolumeInfo"
@@ -68,13 +60,14 @@ class DbCalls(Main):
         drop_saleInfo = "DROP TABLE IF EXISTS SaleInfo"
 
         cursor.execute(drop_IndustryIdentifier)
-
         cursor.execute(drop_saleInfo)
         cursor.execute(drop_publish)
         cursor.execute(drop_VolumeInfo)
         cursor.execute(drop_bookInfo)
         cursor.execute(drop_book)
+    def table_creation(self, cursor):
 
+        self.drop_if_exists(cursor)
         cursor.execute(
             "CREATE TABLE Book "
             "(bookId varchar(255) NOT NULL, "
@@ -85,19 +78,20 @@ class DbCalls(Main):
 
         cursor.execute(
             "CREATE TABLE BookInfo "
-            "(bookId varchar(255) NOT NULL, "
+            "(bookInfoId varchar(255) NOT NULL, "
             "etag varchar(255), "
             "selfLink varchar(255), "
             "language varchar(30), "
             "isAvailablePDF varchar(10), "
-            "PRIMARY KEY(bookId), "
+            "bookId varchar(255), "
+            "PRIMARY KEY(bookInfoId), "
             "FOREIGN KEY (bookId) REFERENCES Book(bookId))"
         )
         cursor.execute(
             "CREATE TABLE VolumeInfo "
             "(vId varchar(255) NOT NULL, "
             "title varchar(255), "
-            "subtitle varchar(255), "
+            "subtitle varchar(1000), "
             "publishedDate varchar(255), "
             "bookId varchar(255), "
             "PRIMARY KEY(vId), "
@@ -135,88 +129,99 @@ class DbCalls(Main):
             "PRIMARY KEY (industryId), "
             "FOREIGN KEY (bookId) REFERENCES BookInfo(bookId))"
         )
+    def get_params(self, new_data, i):
+        bookId = str(uuid.uuid4())
+        totalItem = random.randint(1, 100)
+        etag = main.validate_string(new_data[i]["etag"])
+        selfLink = main.validate_string((new_data[i]["selfLink"]))
+        language = main.validate_string(new_data[i]["volumeInfo"]["language"])
+        isAvailablePDF = main.validate_string(
+            new_data[i]["accessInfo"]["pdf"]["isAvailable"]
+        )
+        vId = str(uuid.uuid4())
+        title = main.validate_string(new_data[i]["volumeInfo"]["title"])
+        try:
+            subtitle = main.validate_string(new_data[i]["volumeInfo"]["subtitle"])
+        except:
+            subtitle = "null"
 
-    # def fetchParameters(self, new_data):
-    def data_insertion_into_table(self, new_data):
-        # parse json data to SQL insert
-        type = url.split("?")[1].split("=")[1].split(":")[0]
-
-        for i in range(len(new_data)):
-
-            bookId = main.validate_string(new_data[i]["id"])
-            totalItem = random.randint(1, 100)
-            etag = main.validate_string(new_data[i]["etag"])
-            selfLink = main.validate_string((new_data[i]["selfLink"]))
-            language = main.validate_string(new_data[i]["volumeInfo"]["language"])
-            isAvailablePDF = main.validate_string(
-                new_data[i]["accessInfo"]["pdf"]["isAvailable"]
+        try:
+            publishedDate = main.validate_string(
+                new_data[i]["volumeInfo"]["publishedDate"]
             )
-            vId = str(uuid.uuid4())
-            title = main.validate_string(new_data[i]["volumeInfo"]["title"])
-            try:
-                subtitle = main.validate_string(new_data[i]["volumeInfo"]["subtitle"])
-            except:
-                subtitle = "null"
+        except:
+            publishedDate = "null"
 
-            try:
-                publishedDate = main.validate_string(
-                    new_data[i]["volumeInfo"]["publishedDate"]
-                )
-            except:
-                publishedDate = "null"
+        pId = str(uuid.uuid4())
+        try:
+            publisher = main.validate_string(new_data[i]["volumeInfo"]["publisher"])
+        except:
+            publisher = "Concordia University"
 
-            pId = str(uuid.uuid4())
-            try:
-                publisher = main.validate_string(new_data[i]["volumeInfo"]["publisher"])
-            except:
-                publisher = "Concordia University"
+        try:
+            description = main.validate_string(
+                new_data[i]["volumeInfo"]["description"]
+            )
+        except:
+            description = "No description available"
 
-            try:
-                description = main.validate_string(
-                    new_data[i]["volumeInfo"]["description"]
-                )
-            except:
-                description = "No description available"
-
-            sId = str(uuid.uuid4())
-            countryChoice = ["CA", "US", "IN", "GE", "UK", "AU"]
-            country = random.choice(countryChoice)
-            saleAbility = main.validate_string(new_data[i]["saleInfo"]["saleability"])
-            isEbook = main.validate_string(new_data[i]["saleInfo"]["isEbook"])
-            industryId = str(uuid.uuid4())
-            typeOfIdentifier = ""
-            identifier = ""
-            try:
-                for j in range(len(new_data[i]["volumeInfo"]["industryIdentifiers"])):
-                    try:
-                        typeOfIdentifier += (
+        sId = str(uuid.uuid4())
+        countryChoice = ["CA", "US", "IN", "GE", "UK", "AU"]
+        country = random.choice(countryChoice)
+        saleAbility = main.validate_string(new_data[i]["saleInfo"]["saleability"])
+        isEbook = main.validate_string(new_data[i]["saleInfo"]["isEbook"])
+        industryId = str(uuid.uuid4())
+        typeOfIdentifier = ""
+        identifier = ""
+        try:
+            for j in range(len(new_data[i]["volumeInfo"]["industryIdentifiers"])):
+                try:
+                    typeOfIdentifier += (
                             main.validate_string(
                                 new_data[i]["volumeInfo"]["industryIdentifiers"][j]["type"]
-                            )+ ", "
-                        )
-                        identifier += (
+                            ) + ", "
+                    )
+                    identifier += (
                             main.validate_string(
                                 new_data[i]["volumeInfo"]["industryIdentifiers"][j]["identifier"]
-                            )+ ", "
-                        )
-                    except:
-                        typeOfIdentifier += ""
-                        identifier += ""
-            except:
-                pass
+                            ) + ", "
+                    )
+                except:
+                    typeOfIdentifier += ""
+                    identifier += ""
+        except:
+            pass
 
-            if typeOfIdentifier != "":
-                typeOfIdentifier = typeOfIdentifier[:-2]
-                identifier = identifier[:-2]
+        if typeOfIdentifier != "":
+            typeOfIdentifier = typeOfIdentifier[:-2]
+            identifier = identifier[:-2]
 
+        return bookId, totalItem, etag, selfLink, language, isAvailablePDF, vId, title, subtitle, publishedDate, pId, publisher, publishedDate, description, sId, country, saleAbility, isEbook, industryId, typeOfIdentifier, identifier
+    def data_insertion_into_table(self, new_data, cursor):
+        # parse json data to SQL insert
+
+        bookId = str(uuid.uuid4())
+        type = url.split("?")[1].split("=")[1].split(":")[0]
+        totalItems = random.randint(1, 40)
+
+        cursor.execute(
+            "INSERT INTO Book (bookId, type, totalItems) VALUES (%s, %s, %s)",
+            (bookId, type, totalItems),
+        )
+        connection.db1.commit()
+
+        for i in range(len(new_data)):
+            bookId, totalItem, etag, selfLink, language, isAvailablePDF, vId, title, subtitle, publishedDate, pId, publisher, publishedDate, description, sId, country, saleAbility, isEbook, industryId, typeOfIdentifier, identifier = self.get_params(
+                new_data, i)
+            sql = f"SELECT bookId FROM book where type = '{type}'"
+            cursor.execute(sql)
+
+
+            bookId = cursor.fetchone()[0]
+            bookInfoId = str(uuid.uuid4())
             cursor.execute(
-                "INSERT INTO Book (bookId, type, totalItems) VALUES (%s, %s, %s)",
-                (bookId, type, totalItem),
-            )
-
-            cursor.execute(
-                "INSERT INTO BookInfo (bookId, etag, selfLink, language, isAvailablePDF) VALUES (%s, %s, %s, %s, %s)",
-                (bookId, etag, selfLink, language, isAvailablePDF),
+                "INSERT INTO BookInfo (bookInfoId, etag, selfLink, language, isAvailablePDF, bookId) VALUES (%s, %s, %s, %s, %s, %s)",
+                (bookInfoId, etag, selfLink, language, isAvailablePDF, bookId),
             )
 
             cursor.execute(
@@ -226,7 +231,7 @@ class DbCalls(Main):
 
             cursor.execute(
                 "INSERT INTO Publish (pId, publisher, publishedDate, description, bookId) VALUES (%s, %s, %s, %s, %s)",
-                (pId, publisher, publishedDate, description, bookId),
+                (pId, publisher, publishedDate, description[:1000], bookId),
             )
 
             cursor.execute(
@@ -238,24 +243,12 @@ class DbCalls(Main):
                 "INSERT INTO IndustryIdentifier (industryId, type, identifier, bookId) VALUES (%s, %s, %s, %s)",
                 (industryId, typeOfIdentifier, identifier, bookId),
             )
-        print("Data inserted successfully into VolumeInfo")
 
         connection.db1.commit()
-
+        print("Data inserted successfully into all the table")
 
 if __name__ == "__main__":
-    # Read API data into JSON
     main = Main()
-    json_data = main.read_api()
-    # Create a api_data.json file and put downloaded data into that file
-    main.write_data(json_data)
-    # Connect with DB
-    cursor = main.connect()
-    # Read data from downloaded file
-    data = main.read_data()
-    # Table creation
-    dbCalls = DbCalls()
-    dbCalls.table_creation(cursor)
 
-    # insert data into table
-    dbCalls.data_insertion_into_table(data)
+    # Read API data into JSON
+    main.read_api()
